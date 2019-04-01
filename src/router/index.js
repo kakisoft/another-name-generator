7bf6e28f27fad1f75f9b69bdc8c68691c1656ec8
kakisoft <kakisttab@gmail.com>
2019-04-02 03:40:38 +0900

    e

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,14 @@ import AddonsPage from '../docs/categories/AddonsPage';
 
 // FREE
 import BlogComponentsPage from '../docs/BlogComponentsPage';
-import DropdownPage from '../docs/DropdownPage';
 import EdgeHeaderPage from '../docs/EdgeHeaderPage';
 import GridPage from '../docs/GridPage';
 import HomePage from '../docs/HomePage';
 import IframePage from '../docs/IframePage';
-import JumbotronPage from '../docs/JumbotronPage';
 import LiveDemoPage from '../docs/LiveDemoPage';
-import ListGroupPage from '../docs/ListGroupPage';
 import MediaPage from '../docs/MediaPage';
 import PaginationPage from '../docs/PaginationPage';
 import PanelPage from '../docs/PanelPage';
-import ProgressBarsPage from '../docs/ProgressBarsPage';
-import SliderPage from '../docs/SliderPage';
-import SpinnersPage from '../docs/SpinnersPage';
-import TabsPage from '../docs/TabsPage';
-import TablePage from '../docs/TablePage';
 import VideoPage from '../docs/VideoPage';
 
 Vue.use(Router);
@@ -48,21 +40,9 @@ export default new Router({
       name: 'GridPage',
       component: GridPage
     }, {
-      path: '/css/table',
-      name: 'TablePage',
-      component: TablePage
-    }, {
       path: '/plugins/iframe',
       name: 'IframePage',
       component: IframePage
-    }, {
-      path: '/components/dropdown',
-      name: 'DropdownPage',
-      component: DropdownPage
-    }, {
-      path: '/components/jumbotron',
-      name: 'JumbotronPage',
-      component: JumbotronPage
     }, {
       path: '/components/liveDemo',
       name: 'LiveDemoPage',
@@ -72,10 +52,6 @@ export default new Router({
       name: 'MediaPage',
       component: MediaPage
     }, {
-      path: '/components/listgroup',
-      name: 'ListGroupPage',
-      component: ListGroupPage
-    }, {
       path: '/components/pagination',
       name: 'PaginationPage',
       component: PaginationPage
@@ -84,29 +60,13 @@ export default new Router({
       name: 'PanelPage',
       component: PanelPage
     }, {
-      path: '/components/progress-bars',
-      name: 'ProgressBarsPage',
-      component: ProgressBarsPage
-    }, {
       path: '/components/edge-header',
       name: 'EdgeHeaderPage',
       component: EdgeHeaderPage
     }, {
-      path: '/components/slider',
-      name: 'SliderPage',
-      component: SliderPage
-    }, {
-      path: '/components/tabs',
-      name: 'TabsPage',
-      component: TabsPage
-    }, {
       path: '/plugins/video',
       name: 'VideoPage',
       component: VideoPage
-    }, {
-      path: '/components/spinners',
-      name: 'SpinnersPage',
-      component: SpinnersPage
     }, {
       path: '/plugins/blog-components',
       name: 'BlogComponentsPage',
