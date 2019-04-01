452ef8b01581fc4b3eda7a1240f2dc0748314439
kakisoft <kakisttab@gmail.com>
2019-04-02 04:04:00 +0900

    e

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,8 @@ import AddonsPage from '../docs/categories/AddonsPage';
 
 // FREE
 import BlogComponentsPage from '../docs/BlogComponentsPage';
-import EdgeHeaderPage from '../docs/EdgeHeaderPage';
-import GridPage from '../docs/GridPage';
 import HomePage from '../docs/HomePage';
 import IframePage from '../docs/IframePage';
-import LiveDemoPage from '../docs/LiveDemoPage';
-import MediaPage from '../docs/MediaPage';
-import PaginationPage from '../docs/PaginationPage';
-import PanelPage from '../docs/PanelPage';
 import VideoPage from '../docs/VideoPage';
 
 Vue.use(Router);
@@ -36,33 +30,9 @@ export default new Router({
 
     // FREE
     {
-      path: '/components/grid',
-      name: 'GridPage',
-      component: GridPage
-    }, {
       path: '/plugins/iframe',
       name: 'IframePage',
       component: IframePage
-    }, {
-      path: '/components/liveDemo',
-      name: 'LiveDemoPage',
-      component: LiveDemoPage
-    }, {
-      path: '/components/media',
-      name: 'MediaPage',
-      component: MediaPage
-    }, {
-      path: '/components/pagination',
-      name: 'PaginationPage',
-      component: PaginationPage
-    }, {
-      path: '/components/panel',
-      name: 'PanelPage',
-      component: PanelPage
-    }, {
-      path: '/components/edge-header',
-      name: 'EdgeHeaderPage',
-      component: EdgeHeaderPage
     }, {
       path: '/plugins/video',
       name: 'VideoPage',
